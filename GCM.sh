04f3579189098abbce8845a2221ef7dbd6c5abdf
null-ecp <34653840+null-ecp@users.noreply.github.com>
2017-12-23 19:04:45 +0800

    Update GCM.sh

--- GCM.sh
+++ GCM.sh
@@ -19,7 +19,7 @@ Debian(){  #Debian install about
 		libnet-ssleay-perl libio-socket-ssl-perl
 }
 Uninstall(){
-	echo 'Uninstall FFM Server,Exiting...'
+	echo 'Uninstall GCM Server,Exiting...'
 	sleep 3s
 	exit
 }
@@ -43,6 +43,21 @@ $client->load("GCM",data=>{
     #此处为讨论组，填写格式同上
 });">>/root/qq.pl
 }
+MojowxGCM(){
+	echo "use Mojo::Weixin;
+my $client = Mojo::Weixin->new(log_encoding=>"utf-8");
+$client->load("ShowMsg");
+$client->load("GCM",data=>{
+    api_url => 'https://gcm-http.googleapis.com/gcm/send',
+    api_key=>'AIzaSyB18io0hduB_3uHxKD3XaebPCecug27ht8',
+    registration_ids=>["输入你自己从 GCMForMojo APP中获取到的令牌"],
+    allow_group=>["接收消息的群名称，不填则接收全部"", 分隔"],
+    ban_group=>[],
+    allow_discuss=>[],
+    ban_discuss=>[],
+    #此处为讨论组，填写格式同上
+});">>/root/wx.pl
+}
 Mojoqqmi(){
 	echo "use Mojo::Webqq;
 my $client = Mojo::Webqq->new(log_encoding=>"utf-8");
@@ -56,24 +71,42 @@ $client->load("MiPush",data=>{
     #此处为讨论组，填写格式同上
 });">>/root/qq.pl
 }
+Mojowxmi(){
+	echo "use Mojo::Weixin;
+my $client = Mojo::Weixin->new(log_encoding=>"utf-8");
+$client->load("ShowMsg");
+$client->load("MiPush",data=>{
+    registration_ids=>[""],
+    allow_group=>["接收群消息的名称，如需要推送全部群消息可删除这一行，每个群号码之间使用 "", 分隔"],
+    ban_group=>[],
+    allow_discuss=>[],
+    ban_discuss=>[],
+    #此处为讨论组，填写格式同上
+});">>/root/wx.pl
+}
 Mojoqqopen(){
 	echo "$client->load("Openqq",data=>{
     listen => [{host=>"0.0.0.0",port=>5000}, ] ,
-    #如果是推送微信的话需要保证端口不重复，并请保证所设定的端口已经在防火墙内放行，同时需要在 APP 内设定好推送服务器的地址和端口
 });
 #不需要 APP 内回复功能请删除以上三行（不包括被 # 号注释掉的几行）
 $client->run();">>/root/qq.pl
 }
-if [ -f "/root/qq.pl" ]; then
-	rm -rf /root/qq.pl
-else
-	touch /root/qq.pl
-fi
-echo '是否安装Mojoqq(y/n)'
-read select
-if [ $select = y ]; then
-	MojoqqGCM
-	Mojoopen
-else
-	exit
+Mojowxopen(){
+	echo "$client->load("Openwx",data=>{
+    listen => [{host=>"0.0.0.0",port=>5000}, ] ,
+});
+#不需要 APP 内回复功能请删除以上三行（不包括被 # 号注释掉的几行）
+$client->run();">>/root/wx.pl
+}
+delqq(){
+	if [ -f "/root/qq.pl" ]; then
+		rm -rf /root/qq.pl
+	else
+		touch /root/qq.pl
+	fi
+}
+echo 'are you want del qq?(y/n)'
+read result
+if [ $result = y ]; then
+	delqq
 fi
