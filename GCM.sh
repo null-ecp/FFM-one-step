52b363505c4870b78790a0cd0d6d98408ed6b6b7
null-ecp <34653840+null-ecp@users.noreply.github.com>
2017-12-23 18:24:34 +0800

    Update GCM.sh

--- GCM.sh
+++ GCM.sh
@@ -28,12 +28,10 @@ Selecterr(){
 	sleep 3s
 	exit
 }
-Mojoqq(){
-	echo 'use Mojo::Webqq;
+MojoqqGCM(){
+	echo "use Mojo::Webqq;
 my $client = Mojo::Webqq->new(log_encoding=>"utf-8");
 $client->load("ShowMsg");
-#请根据自己所需的推送服务进行选择并删除或注释不需要的部分，填写格式请仿照 GCM 的方式填写
-#以下为 GCM 推送
 $client->load("GCM",data=>{
     api_url => 'https://gcm-http.googleapis.com/gcm/send',
     api_key=>'AIzaSyB18io0hduB_3uHxKD3XaebPCecug27ht8',
@@ -43,32 +41,39 @@ $client->load("GCM",data=>{
     allow_discuss=>[],
     ban_discuss=>[],
     #此处为讨论组，填写格式同上
-});
-#以下为 MiPush 推送
+});">>/root/qq.pl
+}
+Mojoqqmi(){
+	echo "use Mojo::Webqq;
+my $client = Mojo::Webqq->new(log_encoding=>"utf-8");
+$client->load("ShowMsg");
 $client->load("MiPush",data=>{
     registration_ids=>[""],
-    allow_group=>[""],
+    allow_group=>["接收群消息的号码，如需要推送全部群消息可删除这一行，每个群号码之间使用 "", 分隔"],
     ban_group=>[],
     allow_discuss=>[],
     ban_discuss=>[],
-});
-$client->load("Openqq",data=>{
+    #此处为讨论组，填写格式同上
+});">>/root/qq.pl
+}
+Mojoqqopen(){
+	echo "$client->load("Openqq",data=>{
     listen => [{host=>"0.0.0.0",port=>5000}, ] ,
     #如果是推送微信的话需要保证端口不重复，并请保证所设定的端口已经在防火墙内放行，同时需要在 APP 内设定好推送服务器的地址和端口
 });
 #不需要 APP 内回复功能请删除以上三行（不包括被 # 号注释掉的几行）
-$client->run();'>>~/qq.pl
+$client->run();">>/root/qq.pl
 }
-
+if [ -f "/root/qq.pl" ]; then
+	rm -rf /root/qq.pl
+else
+	touch /root/qq.pl
+fi
 echo '是否安装Mojoqq(y/n)'
 read select
 if [ $select = y ]; then
-	if [ -f $file ]; then
-		rm -rf $file
-	else 
-		touch $file
-	fi
-	Mojoqq
+	MojoqqGCM
+	Mojoopen
 else
 	exit
 fi
